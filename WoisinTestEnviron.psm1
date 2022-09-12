a3c556b84097acedc5abd64705a4601df23a1794
Nwoisin <nicholas.woisin@outlook.com>
2022-09-12 19:01:33 -0400

    Module Build for MSSA

---
+++ WoisinTestEnviron.psm1
@@ -0,0 +1,30 @@
+# Module Built for Adatum.com Lab Environment during MSSA PSCA-7.  All code is adatped to a lab enviornment
+# and is not currently suitable for a production environment, however, like all PowerShell, it can
+# be adapted.
+
+#Function to shortcut "Get-ADUser"
+Function Get-User {
+        [cmdletbinding()]
+        Param(
+            [Parameter(Mandatory=$true)]
+            [string]$identity
+                      )
+        Get-ADUser -Indentity $identity
+    }
+
+# This will allow you to simply use, for example, Get-User Logan to retrieve an account
+
+# Create a New Organizational Unit via New-OU
+function New-OU
+{
+    Param(
+        [Parameter(Mandatory=$true)][string]$OUname
+            )
+    New-ADOrganizationalUnit -name $OUname -path "dc=Adatum,dc=com"
+}
+# Allows for the creation of an OU with much less time.
+
+##Locate disabled AD Accounts,  Move to disabled OU. .
+
+$DisabledAccounts = "OU=DisabledAccounts,DC=Adatum,DC=com"
+Search-ADAccount -AccountDisabled -UsersOnly | Where-Object DistinguishedName -notlike "*$DisabledAccounts" | Move-ADObject -TargetPath $DisabledAccounts
